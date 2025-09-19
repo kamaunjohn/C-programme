0f10fd4c9cfc0840238d403015411c3bd72514bf
kamaunjohn <njerikamau493@gmail.com>
2025-09-19 16:07:06 +0300

    Add files via upload

---
+++ 20250917-221653.c
@@ -0,0 +1,23 @@
+//c programme
+/*
+Name:John
+Reg:CT100/G/26208/25
+description:to prompt the student to
+entre height,weight,phone number
+*/
+
+#include<stdio.h>
+int main(){
+int height,weight,phone;
+printf("entre your height");
+scanf("%d",&height);
+printf("entre your weight");
+scanf("%d",&weight);
+printf("entre your phone");
+scanf("%d",&phone);
+printf("height;%d",height);
+printf("weight;%d",weight);
+printf("phone;%d",phone);
+}
+
+    
