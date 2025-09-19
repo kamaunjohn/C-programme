392892b454d0b72081a66299b63f1ce8472c3b1c
kamaunjohn <njerikamau493@gmail.com>
2025-09-20 00:33:50 +0300

    Add files via upload

---
+++ Jk254.c
@@ -0,0 +1,23 @@
+//c programme (hello world)single line assignment
+/*
+Name:John
+Reg no:CT100/G/26208/25
+Description:hello c
+*/
+#include<stdio.h>pre-proccesor directive
+
+//main function
+int main(){
+printf("semester 1 units,\n");
+printf("introduction to programming and algorithm,\n");
+printf("analogue electronics,\n");
+printf("introduction to computer applications,\n");
+printf("communication and literacy skills,\n");
+printf("mathematics for science,\n");
+printf("essentials of accounting,\n");
+printf("computer systems and organisation,\n");
+printf("fundamentals of ICT,\n");
+
+}
+ 
+
